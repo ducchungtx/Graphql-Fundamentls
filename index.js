273720a48ab6c3736fddaba9ce1889688006f4dc
Chung Nguyen <zrmedia9@gmail.com>
2018-03-18 16:13:52 +0700

    setup es6

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    return res.json({
+        msg: "Welcome to GraphQL World!"
+    });
+})
+
 app.listen(3000, () => {
     console.log('App is running on PORT 3000');
-});
+})
