fed869c072c855d6502f7670011d5024946da0b9
Chung Nguyen <zrmedia9@gmail.com>
2018-03-18 16:37:07 +0700

    creating graphql schema

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
 
 const app = express();
 
@@ -7,6 +9,16 @@ app.get('/', (req, res) => {
         msg: "Welcome to GraphQL World!"
     });
 })
+
+const root = {
+    hello: () => 'Hello welcome to GraphQL'
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
 
 app.listen(3000, () => {
     console.log('App is running on PORT 3000');
